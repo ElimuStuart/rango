af3461d836312af7ea2482235dd66b7fc7b991d1
stue <stuartelimu@gmail.com>
2018-08-30 11:54:27 +0300

    change application = get_wsgi_application() to application = Cling(get_wsgi_application()) in wsgi.py

--- tango_with_django_project/wsgi.py
+++ tango_with_django_project/wsgi.py
@@ -15,4 +15,4 @@ from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tango_with_django_project.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
